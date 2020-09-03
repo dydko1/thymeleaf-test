a69dcccd4c7eca54e09613a8ed2bcae41d6b0dbe
dydko1 <miroslaw.dyduch@gmail.com>
2020-09-03 13:03:20 +0200

    commit 03

--- src/main/java/com/dydko/demo/HelloController.java
+++ src/main/java/com/dydko/demo/HelloController.java
@@ -1,13 +1,15 @@
 package com.dydko.demo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String getCar()
-    {
+    public String get(Model model) {
+        Car car = new Car("Renault", "Megane");
+        model.addAttribute("car", car);
         return "hello";
     }
 }
