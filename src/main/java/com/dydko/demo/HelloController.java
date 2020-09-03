d8dcc57f63007a9fee33a6fe7641b8f9480b8f24
dydko1 <miroslaw.dyduch@gmail.com>
2020-09-03 13:27:40 +0200

    abc

--- src/main/java/com/dydko/demo/HelloController.java
+++ src/main/java/com/dydko/demo/HelloController.java
@@ -3,13 +3,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class HelloController {
 
+    private List<Car> cars;
+
+    public HelloController(List<Car> cars) {
+        Car car1 = new Car("Renault1", "Megane1");
+        Car car2 = new Car("Renault2", "Megane2");
+        Car car3 = new Car("Renault3", "Megane3");
+
+        this.cars = Arrays.asList(car1, car2, car3);
+    }
+
     @GetMapping("/hello")
     public String get(Model model) {
-        Car car = new Car("Renault1", "Megane1");
-        model.addAttribute("car", car);
+
+        model.addAttribute("cars", cars);
         return "hello";
     }
 }
