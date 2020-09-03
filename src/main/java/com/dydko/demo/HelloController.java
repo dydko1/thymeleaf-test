25c9b06fb4f146390ba86d683202b796023e1b00
dydko1 <miroslaw.dyduch@gmail.com>
2020-09-03 09:22:22 +0200

    1

--- src/main/java/com/dydko/demo/HelloController.java
+++ src/main/java/com/dydko/demo/HelloController.java
@@ -1,4 +1,13 @@
 package com.dydko.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String getCar()
+    {
+        return "hello";
+    }
 }
